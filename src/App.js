035287306298637a4bdffa5f162ef9ec92a16ab5
Jacob Johnson <jacobjeremiah42@gmail.com>
2023-05-10 02:08:07 -0400

    Working navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react"
 import './App.css'
 import Navbar from "./components/Navbar.js"
+import Home from "./components/Home.js"
+import About from "./components/About.js"
+import Projects from "./components/Projects.js"
+import Resume from "./components/Resume.js"
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/projects' element={<Projects />} />
+        <Route exact path='/resume' element={<Resume />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,41 @@
 import React from "react"
-import Title from "./Title.js"
+import { NavLink } from "react-router-dom"
 
 export default function Navbar(){
+
+    function clicked(e) {
+        const navbarList = e.target.parentElement.children
+        for (let i = 0; i < navbarList.length; i++){
+            navbarList[i].id = ""
+        }
+        let currentPage = e.target.innerText
+        e.target.id = "selected"
+        if (currentPage === "Home"){
+            window.location.href = "/"
+        } else {
+            window.location.href = "/" + currentPage.toLowerCase()
+        }
+    }
+
     return (
-        <div className="navbar">
-            <ul className="navbar--list">
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Portfolio</li>
-            </ul>
+        <div>
+            <div className="navbar">
+                <div className="navbar--list">
+                    <NavLink
+                    to="/" >
+                        <div>Home</div>
+                    </NavLink>
+                    <NavLink to="/about">
+                        <div>About</div>
+                    </NavLink>
+                    <NavLink to="/projects">
+                        <div>Projects</div>
+                    </NavLink>
+                    <NavLink to="/resume">
+                        <div>Resume</div>
+                    </NavLink>
+                </div>
+            </div>
         </div>
     )
 }
